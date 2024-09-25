2958c0d6f4427603a8106b32cbad547491921daf
Ena224 <ena24.nwu@gmail.com>
2024-09-25 12:35:48 +0600

    question part added

--- js/blog.js
+++ js/blog.js
@@ -2,5 +2,5 @@ document.getElementById('blog-btn')
   .addEventListener('click',function(event){
     event.preventDefault();
      console.log('Blog button clicked ! ');
-     window.location.href = '/home.html'
+     window.location.href = '/blog.html'
   })
