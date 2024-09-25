4a74c3da1732cced0d95031a6b9eee3e04ef230e
Ena224 <ena24.nwu@gmail.com>
2024-09-25 13:20:33 +0600

    added

--- js/blog.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('blog-btn') 
-  .addEventListener('click',function(event){
-    event.preventDefault();
-     console.log('Blog button clicked ! ');
-     window.location.href = '/blog.html'
-  })
